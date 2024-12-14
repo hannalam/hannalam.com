5e1d69659593b0a6b0d5a6d370a85abc73e42fa5
hanna lam <63362052+hannalam@users.noreply.github.com>
2024-12-14 19:09:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/hannalam.com/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
